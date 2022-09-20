77a329adb49dcf80e803941c07a8b3be498df52a
Marcus-Giglio <gigliomarcus9@gmail.com>
2022-09-20 11:48:03 +0200

    general update

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Stats preview card component solution
+
+This is a solution to the [Stats preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+
+### Screenshot
+
+![](./src/assets/Screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React Typescript](https://www.typescriptlang.org/docs/handbook/react.html) - JS library
+- [Bootstrap](https://getbootstrap.com/docs/5.2/getting-started/introduction/) - For styles
+
+### Continued development
+
+I want to continue to learn more about React and Typescript.
+
+## Author
+
+- Frontend Mentor - [@Marcus-Giglio](https://www.frontendmentor.io/profile/Marcus-Giglio)
+- Twitter - [@Marcus Giglio](https://www.linkedin.com/in/marcus-g-514b51175/)
--- src/components/Card/Index.tsx
+++ src/components/Card/Index.tsx
@@ -2,16 +2,16 @@ import imgContainer from "../../assets/img-container.png";
 
 const Card = () => {
   return (
-    <div className="row gx-1">
-      <div className="col-12 col-xl-6 p-0 order-xl-2 img-wrapper">
+    <div className="row gx-0">
+      <div className="col-12 col-xl-6 p-0 order-xl-2 img-wrapper border-radius-top">
         <img
           src={imgContainer}
           alt="image-header"
           className="img-fluid border-radius-top"
         />
       </div>
-      <div className="col-12 col-xl-6 d-xl-flex flex-xl-column justify-content-xl-center card-body">
-        <div className="row">
+      <div className="col-12 col-xl-6 d-xl-flex flex-xl-column justify-content-xl-center px-xl-4 card-body">
+        <div className="row px-5 pb-4 pt-5">
           <div className="col text-center text-xl-start">
             <h1 className="fw-bold text-white m-0 inter">
               Get <span style={{ color: "hsl(277, 64%, 61%)" }}>inisghts</span>{" "}
@@ -19,7 +19,7 @@ const Card = () => {
             </h1>
           </div>
         </div>
-        <div className="row">
+        <div className="row px-5 pb-4 pb-xl-5">
           <div className="col text-center text-xl-start">
             <p className="main-paragraph m-0 inter">
               Discover the benefits of data analytics and make better decisions
@@ -27,17 +27,17 @@ const Card = () => {
             </p>
           </div>
         </div>
-        <div className="row flex-column flex-xl-row">
-          <div className="col text-center">
-            <h1 className="fw-bold text-white inter">10k+</h1>
+        <div className="row px-5 flex-column flex-xl-row gy-4 pt-4 pb-5">
+          <div className="col text-center text-xl-start">
+            <h2 className="fw-bold text-white inter">10k+</h2>
             <p className="stat-heading lexend m-0 ">COMPANIES</p>
           </div>
-          <div className="col text-center">
-            <h1 className="fw-bold text-white inter">314</h1>
+          <div className="col text-center text-xl-start">
+            <h2 className="fw-bold text-white inter">314</h2>
             <p className="stat-heading lexend m-0 ">TEMPLATES</p>
           </div>
-          <div className="col text-center pb-4">
-            <h1 className="fw-bold text-white inter">12M+</h1>
+          <div className="col text-center text-xl-start">
+            <h2 className="fw-bold text-white inter">12M+</h2>
             <p className="stat-heading lexend m-0 ">QUERIES</p>
           </div>
         </div>
